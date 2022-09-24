587f022c91de3d39ad3b1b21f8f6371e160acac9
sossup <davibarguarti@gmail.com>
2022-09-24 16:08:22 -0300

    atualizando o arquivo

--- exibir.js
+++ exibir.js
@@ -1,4 +1,6 @@
 import { Carro } from './Carro.js';
+import { Veiculo } from './organizandoClassPai.js';
+import { Moto } from './Moto.js'
 
 // const novoCarro = new Carro('ford','5','','4','verde','direita');
 // console.log(novoCarro.marca);
