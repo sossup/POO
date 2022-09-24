96330a9417fa0b024cd93ad24e6fb80b7870cab8
sossup <davibarguarti@gmail.com>
2022-09-24 15:56:51 -0300

    Organizando class filho

--- Carro.js
+++ Carro.js
@@ -1,7 +1,19 @@
-import { Veiculo } from './Veiculo.js';
+import { Veiculo } from './organizandoClassPai.js';
 
 export class Carro extends Veiculo {
+    #porta
+    #ladoVolante
     constructor(marca, porta, motor = true, roda, cor, ladoVolante){
-        super (marca, porta, motor = true, roda, cor, ladoVolante,);
+        super (marca, motor = true, roda, cor);
+        this.#ladoVolante = ladoVolante;
+        this.#porta = porta;
+    }
+
+    get ladoVolante(){
+        return ladoVolante = this.#ladoVolante
+    }
+
+    get porta(){
+        return porta = this.#porta
     }
 }
